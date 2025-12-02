acfdbdd1b46fe7d4c7f75ea169031dbdbd58569e
Ilya-Cisek <surovtsevhome@gmail.com>
2025-12-03 00:52:51 +0300

    moonfly theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,13 @@
 return {
-  'folke/tokyonight.nvim',
-  priority = 1000,
-  config = function()
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false },
-      },
-    }
-    vim.cmd.colorscheme 'tokyonight-night'
-  end,
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        -- Enable transparent background for Moonfly
+        vim.g.moonflyTransparent = true
+        vim.cmd.colorscheme "moonfly"
+        -- Optional: Also set floating windows to be transparent
+        vim.g.moonflyNormalFloat = true
+    end,
 }
